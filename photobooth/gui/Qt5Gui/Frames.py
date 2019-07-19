82dd2f320f1a34f1377f68d1169b9f060fcf6e26
romaingerardy <rom.gerardy@gmail.com>
2019-07-19 18:08:03 +0200

    add logo

--- photobooth/gui/Qt5Gui/Frames.py
+++ photobooth/gui/Qt5Gui/Frames.py
@@ -50,8 +50,8 @@ class Welcome(QtWidgets.QFrame):
 
         label = QtWidgets.QLabel()
         pixmap = QPixmap('photobooth/gui/Qt5Gui/images/pmb-logo.png')
+        pixmap = pixmap.scaled(400,110,Qt::KeepAspectRatio)
         label.setPixmap(pixmap)
-        label.resize(400,110)
 
         btnStart = QtWidgets.QPushButton(_('Start photobooth'))
         btnStart.clicked.connect(start_action)
